53c39994c3d56268c6b47f3b26e16a46fd39321b
Salman <bnsalman07@gmail.com>
2021-10-30 02:01:38 +0200

    subida archivos 2

--- Js.js
+++ Js.js
@@ -1,20 +1,16 @@
-const Nombre = document.querySelector('.nombre');
 const User = document.querySelector('.usuario');
 const login = document.querySelector('.enviar');
 const pswd = document.querySelector('.constraseña');
-const registrar = document.querySelector('.registro')
 
 login.addEventListener("click", (e) =>{
 	e.preventDefault();
-	if(User.length>=4 || pswd.length>=6){
+	const text = User.value;
+
+	if(text.length>=4){
 		window.alert("Inicio de sesion correcto");
 	}
 	else{
-		window.alert("Clave o Usuario incorrecto");
+		window.alert("");
 	}
 })
 
-registrar.addEventListener("click", (e) =>{
-	e.preventDefault();
-	window.open("/Registro.html")
-})
